6dac2df093614332658e752360241f4d3c600d0f
Sidhant Mishra <mishrasidhant01@gmail.com>
2023-09-30 20:12:06 +0530

    Completed project.py

--- project.py
+++ project.py
@@ -3,27 +3,29 @@ import requests
 import time
 
 mySkills = input("Enter your skills> ")
-html_txt = requests.get(
+def findJobs():
+    html_txt = requests.get(
     "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation="
-).text
-# print(html_txt)
-
-soup = BeautifulSoup(html_txt, "lxml")
-
-jobs = soup.find_all("li", class_="clearfix job-bx wht-shd-bx")
-
-
-print(f"Filtering out jobs that include the skill: {mySkills}\n")
-
-for job in jobs:
-    publishedDate = job.find("span", class_="sim-posted").span.text
+    ).text
     
-    if "few" in publishedDate:
-        skills = job.find("span", class_="srp-skills").text.replace(" ","")
-        if(mySkills in skills):
-            
-            companyName = job.find("h3", class_="joblist-comp-name").text.replace(" ","")
-            moreInfo = job.header.h2.a["href"]
-            print(f"Company Name: {companyName.strip()}\nSkills Required: {skills.strip()}\nMore Info: {moreInfo.strip()}\n")
-            print("\n---------------------------------")
+    soup = BeautifulSoup(html_txt, "lxml")
+    jobs = soup.find_all("li", class_="clearfix job-bx wht-shd-bx")
+    print(f"Filtering out jobs that include the skill: {mySkills}\n")
 
+    for index, job in enumerate(jobs):
+        publishedDate = job.find("span", class_="sim-posted").span.text
+        
+        if "few" in publishedDate:
+            skills = job.find("span", class_="srp-skills").text.replace(" ","")
+            if(mySkills in skills):
+                with open(f'Jobs/Job{index}.txt', 'w') as f:
+                    companyName = job.find("h3", class_="joblist-comp-name").text.replace(" ","")
+                    moreInfo = job.header.h2.a["href"]
+                    f.write(f"Company Name: {companyName.strip()}\nSkills Required: {skills.strip()}\nMore Info: {moreInfo.strip()}\n")
+                    print(f"File saved: Job{index}.txt")
+if __name__ == "__main__":
+    while True:
+        findJobs()
+        time_mins = 10
+        print(f"Waiting {time_mins} minutes...")
+        time.sleep(time_mins * 60)
